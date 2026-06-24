99527661a6fefbed88e2e1acae4e6e84840cadec
ProfZica <zicarelli10@hotmail.com>
2026-06-24 10:49:54 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
+      manifest: {
+        name: 'ClassDesk',
+        short_name: 'ClassDesk',
+        description: 'Gestione disposizione banchi e rotazione mensile degli studenti',
+        theme_color: '#2c3e6b',
+        background_color: '#f8f4ef',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ]
+})
